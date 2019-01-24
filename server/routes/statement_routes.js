92eb77157cffc3c3aabbded1b3eb14639694e61f
Michielu Menning <michielu@mac-air-33csiastateedu.local>
2019-01-23 21:15:26 -0600

    CRUD for makeshift crud operations

--- server/routes/statement_routes.js
+++ server/routes/statement_routes.js
@@ -1,11 +1,74 @@
-module.exports = function(app, db) {
+var ObjectID = require('mongodb').ObjectID;
+
+
+/** 
+ * CRUD Example 
+ */
+module.exports = function (app, db) {
   // console.log(db)
   // const collection = 
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {
+      '_id': new ObjectID(id)
+    };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({
+          'error': 'An error has occurred'
+        });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {
+      '_id': new ObjectID(id)
+    };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({
+          'error': 'An error has occurred'
+        });
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      }
+    });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {
+      '_id': new ObjectID(id)
+    };
+    const note = {
+      text: req.body.body,
+      title: req.body.title
+    };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+        res.send({
+          'error': 'An error has occurred'
+        });
+      } else {
+        res.send(note);
+      }
+    });
+  });
+
   app.post('/notes', (req, res) => {
-    const note = { text: req.body.body, title: req.body.title };
+    const note = {
+      text: req.body.body,
+      title: req.body.title
+    };
     db.collection('notes').insertOne(note, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
+      if (err) {
+        res.send({
+          'error': 'An error has occurred'
+        });
       } else {
         res.send(result.ops[0]);
       }
--- server/server.js
+++ server/server.js
@@ -8,17 +8,20 @@ const app = express();
 const port = 8000;
 
 //Process URL encoded 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-MongoClient.connect(db.url, {useNewUrlParser: true}, (err, database) => {
+MongoClient.connect(db.url, {
+    useNewUrlParser: true
+}, (err, database) => {
     if (err) return console.log(err)
-                        
+
     // Make sure you add the database name and not the collection name
     let datab = database.db("my-statement-analyst")
 
     require('./routes')(app, datab);
     app.listen(port, () => {
-      console.log('We are live on ' + port);
-    });               
-  })
- 
+        console.log('We are live on ' + port);
+    });
+})
