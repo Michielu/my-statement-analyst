be997052e1e0e4b32ff09e7d5ee705bcf43b5b04
Michielu <mich.menning@gmail.com>
2019-02-02 09:49:42 -0600

    Display transactions on antd table

--- client/src/components/AllTrans/index.js
+++ client/src/components/AllTrans/index.js
@@ -2,6 +2,8 @@ import React, {
   Component
 } from 'react';
 
+import {Table} from 'antd';
+
 import {
   getAll,
 } from '../../couriers/index'
@@ -23,13 +25,37 @@ class Home extends Component {
   }
 
   render() {
+    let dataSource = [];
+    this.state.transactions.forEach((trans)=>{ 
+      trans.key = trans._id
+      dataSource.push(trans);
+    });
+
+    const columns = [{
+      title: 'Notes',
+      dataIndex: 'notes',
+      key: 'notes',
+    }, {
+      title: 'Labels',
+      dataIndex: 'labels',
+      key: 'labels',
+    }, {
+      title: 'DateOfPurchase',
+      dataIndex: 'dateOfPurchase',
+      key: 'dateOfPurchase',
+    }, {
+      title: 'DateOfLog',
+      dataIndex: 'dateOfLog',
+      key: 'dateOfLog',
+    }, {
+      title: 'Cost',
+      dataIndex: 'cost',
+      key: 'cost',
+    }];
+
     return (< div >
-      < h2 > Home </h2>
-      <p>Transactions are: </p>
-      {console.log(this.state.transactions)}
-      {this.state.transactions.map((el) => {
-        return <div key={el._id}>{el.dateOfLog}</div>
-      })}
+      <h2>Transactions are: </h2>
+      <Table dataSource={dataSource} columns={columns} />
     </div>
     );
   }
