71451f990c8c93378652187d831ad96399806477
Michielu <mich.menning@gmail.com>
2019-02-06 08:03:01 -0600

    Rough form for AddTransaction

--- client/src/components/AddTrans/AddTransactions.js
+++ client/src/components/AddTrans/AddTransactions.js
@@ -3,16 +3,10 @@ import React, {
 } from 'react';
 
 import {
-    Card,
-    Button,
-    List,
-    Popconfirm,
-    message
-} from 'antd';
-
-import {
-    deleteTransaction
-} from '../../couriers/'
+    Form, Input, Tooltip, Icon, Button, Select
+  } from 'antd';
+  
+  const { Option } = Select;
 
 
 class AddTransactions extends Component {
@@ -20,6 +14,15 @@ class AddTransactions extends Component {
         super(props);
         this.state = {}
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+          }
+        });
+      }
 
     render() {
         // let trans = this.props.transactions;
@@ -32,15 +35,105 @@ class AddTransactions extends Component {
         //     "Notes: " + trans.notes
         // ];
 
-       
 
+        const { getFieldDecorator } = this.props.form;
+
+        const formItemLayout = {
+            labelCol: {
+              xs: { span: 24 },
+              sm: { span: 8 },
+            },
+            wrapperCol: {
+              xs: { span: 24 },
+              sm: { span: 16 },
+            },
+          };
+
+    const tailFormItemLayout = {
+        wrapperCol: {
+            xs: {
+            span: 24,
+            offset: 0,
+            },
+            sm: {
+            span: 16,
+            offset: 8,
+            },
+        },
+        };
+
+
+        //Cost
+        //Date of purchase
+        //Labels
+        //notes
+
+        // add users 
+        // add date of log
         return ( 
-            <div >
-                <p>Add Transactions</p>
-
-            </div>
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Item
+                 {...formItemLayout}
+                    label={(
+                        <span>
+                        Date of Purchase
+                        </span>
+                    )}
+                >
+                {getFieldDecorator('Date of Purchase')(
+                    <Input style={{ width: '100%' }} />
+                )}
+                </Form.Item>
+                <Form.Item
+                 {...formItemLayout}
+                    label={(
+                        <span>
+                        Labels
+                        </span>
+                    )}
+                >
+                {getFieldDecorator('Labels', {
+                    rules: [{ required: true, message: 'Please input your purchase label/s' }],
+                })(
+                    <Input style={{ width: '100%' }} />
+                )}
+                </Form.Item>
+                <Form.Item
+                 {...formItemLayout}
+                    label={(
+                        <span>
+                       Cost
+                        </span>
+                    )}
+                >
+                {getFieldDecorator('cost', {
+                    rules: [{ required: true, message: 'Please input your purchase cost!' }],
+                })(
+                    <Input style={{ width: '100%' }} />
+                )}
+                </Form.Item>
+                <Form.Item
+                 {...formItemLayout}
+                    label={(
+                        <span>
+                            Notes
+                        </span>
+                    )}
+                >
+                {getFieldDecorator('notes')(
+                    <Input style={{ width: '100%' }} />
+                )}
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                <Button type="primary" htmlType="submit">Add</Button>
+                {/** Create a delete button*/}
+                </Form.Item>
+            </Form>
         );
     }
 }
 
-export default AddTransactions;
+const WrappedAddTransactions = Form.create({ name: 'register' })(AddTransactions);
+
+
+export default WrappedAddTransactions;
