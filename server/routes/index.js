0f23e5b485f5b68aec95d1c10bbe255623c33d67
Michielu <mich.menning@gmail.com>
2019-02-09 10:51:44 -0600

    Server: label requests

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
+const labelRoutes = require('./labels/');
 const statementRoutes = require('./statement_routes');
 const transactionRoutes = require('./transactions/index');
 const userRoutes = require('./users');
 
 
 module.exports = function (app, db) {
+    labelRoutes(app, db);
     statementRoutes(app, db);
     transactionRoutes(app,db);
     userRoutes(app, db);
---
+++ server/routes/labels/index.js
@@ -0,0 +1,79 @@
+var ObjectID = require('mongodb').ObjectID;
+
+//Get all label from user ID
+function getAllLabels(app, db){
+    app.get('/label/a/:userid', (req, res) => {
+        const userID = req.params.userid;
+
+        db.collection('labels').find({
+            user: {
+                $eq: userID
+            }
+        }, (err, cursor) => {
+            if (err) {
+                console.log("err: ", err)
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                cursor.toArray().then((individualTrans) => {
+                    console.log(individualTrans);
+                    res.send(individualTrans);
+                }).catch((e) => {
+                    console.log("err: ", err)
+                    res.send({
+                        "error": "An error has occured"
+                    });
+                })
+            }
+        });
+    })
+}
+
+//Delete label from user 
+function deleteLabel(app, db){
+    app.delete('/label/:id', (req, res) => { 
+        const id = req.params.id;
+        const details = {
+          '_id': new ObjectID(id)
+        };
+        db.collection('labels').remove(details, (err, item) => {
+          if (err) {
+            res.send({
+              'error': 'An error has occurred'
+            });
+          } else {
+            res.send('Label ' + id + ' deleted!');
+          }
+        });
+      });
+}
+
+//Post label from user 
+function createLabel(app, db){
+    app.post('/label', (req, res) => {
+        const label = {
+          user: req.body.user,
+          text: req.body.text
+        };
+        db.collection('labels').insertOne(label, (err, result) => {
+          if (err) {
+            res.send({
+              'error': 'An error has occurred'
+            });
+          } else {
+            res.send(result.ops[0]);
+          }
+        });
+      });
+}
+
+
+
+
+
+module.exports = (app, db) => {
+    createLabel(app, db);
+    deleteLabel(app, db);
+    getAllLabels(app, db);
+}
