e8802ffa62a2a149db370a5937b08b8b9dfd6152
Michielu <mich.menning@gmail.com>
2019-03-08 08:12:55 -0600

    Display dates selected

--- client/src/components/TransRange/TransRange.jsx
+++ client/src/components/TransRange/TransRange.jsx
@@ -8,16 +8,38 @@ const { RangePicker } = DatePicker;
 export default class TransRange extends React.Component {
     state = {
         start: moment(),
-        end: moment()
+        end: moment(),
+        selected: false
     }
 
-    onChange(date, dateString) {
+    onChange = (date, dateString) => {
+        this.setState({
+            start: dateString[0],
+            end: dateString[1],
+            selected: true
+        })
         console.log(date, dateString);
     }
     render() {
-        return (
-            <RangePicker onChange={this.onChange} />
-        )
+
+        if (this.state.selected) {
+            return (
+                <div>
+                    <RangePicker onChange={this.onChange} />
+                    <h2> Dates selected: </h2>
+                    <p>{this.state.start}</p><p> {this.state.end}</p>
+                </div>
+            )
+
+        } else {
+            return (
+                <div>
+                    <RangePicker onChange={this.onChange} />
+                </div>
+            )
+
+        }
+
     }
 
 }
