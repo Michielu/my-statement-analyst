f9c620c316ddab93ff7ff24b0787761f6b3d4966
Michielu <mich.menning@gmail.com>
2019-03-11 08:13:10 -0500

    Use TableFormat in All Transactions

--- client/src/components/AllTrans/AllTransactions.jsx
+++ client/src/components/AllTrans/AllTransactions.jsx
@@ -13,6 +13,10 @@ import {
   convertDate
 } from '../../utils'
 
+import {
+  TableFormat
+} from '../../utils'
+
 
 class AllTransactions extends Component {
   constructor(props) {
@@ -32,91 +36,10 @@ class AllTransactions extends Component {
     });
   }
 
-  findLabelKey = (key) => {
-    for (let i = 0; i < this.state.labels.length; i++) {
-      if (this.state.labels[i]["_id"] == key) {
-        return this.state.labels[i];
-      }
-    }
-  }
-
   render() {
-    let dataSource = [];
-    this.state.transactions.forEach((trans) => {
-      trans.key = trans._id
-      dataSource.push(trans);
-    });
-
-    const columns = [{
-      title: 'Notes',
-      dataIndex: 'notes',
-      key: 'notes',
-    }, {
-      title: 'Labels',
-      dataIndex: 'labels',
-      key: 'labels',
-      render: (labels) => {
-        if (labels) {
-          return (
-            <span>
-              {
-                labels.map(tag => {
-                  const labelTitle = this.findLabelKey(tag);
-                  return <Tag color={'blue'} key={tag}>{labelTitle.text} </Tag>;
-                })
-              }
-            </span>
-          )
-        } else {
-          return (
-            <span>
-              <p>No Label</p>
-            </span>
-          )
-        }
-      }
-    }, {
-      title: 'DateOfPurchase',
-      dataIndex: 'dateOfPurchase',
-      key: 'dateOfPurchase',
-      render: (dop) => {
-        return (
-          <span>
-            <p>{convertDate(dop)}</p>
-          </span>
-        )
-      }
-    }, {
-      title: 'DateOfLog',
-      dataIndex: 'dateOfLog',
-      key: 'dateOfLog',
-      render: (dol) => {
-        return (
-          <span>
-            <p>{convertDate(dol)}</p>
-          </span>
-        )
-      }
-    }, {
-      title: 'Cost',
-      dataIndex: 'cost',
-      key: 'cost',
-    }];
-
-    return (< div >
-      <h2>Transactions are: </h2>
-      <Table onRow={(record) => {
-        return {
-          onClick: () => {
-            this.props.toggleAllTrans(record)
-          },       // click row
-
-        };
-      }}
-        dataSource={dataSource} columns={columns} />
-
-    </div>
-    );
+    return (
+      <TableFormat transactions={this.state.transactions} labels={this.state.labels} toggleAllTrans={this.props.toggleAllTrans} />
+    )
   }
 }
 
