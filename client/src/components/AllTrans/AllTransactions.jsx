9498e61bd157681571ebd6ae246bf54b376f6630
Michielu <mich.menning@gmail.com>
2019-03-18 09:50:13 -0400

    Reduce AllTransactions

--- client/src/components/AllTrans/AllTransactions.jsx
+++ client/src/components/AllTrans/AllTransactions.jsx
@@ -1,46 +1,13 @@
-import React, {
-  Component
-} from 'react';
-
-import { Card, Table, Tag } from 'antd';
-
-import {
-  getAll,
-  getLabels,
-} from '../../couriers/index'
-
-import {
-  convertDate
-} from '../../utils'
+import React from 'react';
 
 import {
   TableFormat
 } from '../../utils'
 
-
-class AllTransactions extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      transactions: [],
-      labels: []
-    }
-  }
-
-  async componentDidMount() {
-    const allTransaction = await getAll();
-    const allLabels = await getLabels();
-    this.setState({
-      transactions: allTransaction.data,
-      labels: allLabels
-    });
-  }
-
-  render() {
-    return (
-      <TableFormat transactions={this.state.transactions} labels={this.state.labels} toggleAllTrans={this.props.toggleAllTrans} />
-    )
-  }
+const AllTransactions = (props) => {
+  return <TableFormat transactions={JSON.parse(sessionStorage.getItem("trans"))}
+    labels={JSON.parse(sessionStorage.getItem("labels"))}
+    toggleAllTrans={props.toggleAllTrans} />
 }
 
 export default AllTransactions;
